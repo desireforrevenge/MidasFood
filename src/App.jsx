d063cf40c5434509bb0f3acf87621c3bac4bebae
who i am <raftalya@inbox.ru>
2024-03-22 12:30:58 +0500

    nav-bar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './styles/default.scss'
+import './styles/home.scss'
+import Nav from './components/Nav'
+
+
+
+export default function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Nav></Nav>
+    </>
+  )
+}
